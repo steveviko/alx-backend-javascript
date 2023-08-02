4d94d26af378958b03e755666a22841835ad9888
steveviko <stevenviko@gmail.com>
2023-08-02 18:45:10 +0000

    Task 0. Creating an interface for a student

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,40 @@
+/** Renders table based on a list of objects */
+
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const studentA: Student = {
+  firstName: 'Steven',
+  lastName: 'Apollo',
+  age: 25,
+  location: 'Kampala-Uganda',
+}
+
+const studentB: Student = {
+  firstName: 'Haron',
+  lastName: 'odongo',
+  age: 21,
+  location: 'Lira-Uganda',
+}
+
+const studentList = [studentA, studentB];
+
+const table = document.createElement('table');
+const tbody = document.createElement('tbody');
+
+studentList.forEach((obj) => {
+  const row = document.createElement('tr');
+  const cellName = document.createElement('td');
+  const cellLocation = document.createElement('td');
+  cellName.textContent = obj.firstName;
+  cellLocation.textContent = obj.location;
+  row.appendChild(cellName);
+  row.appendChild(cellLocation);
+  tbody.appendChild(row);
+});
+table.appendChild(tbody);
+document.body.appendChild(table);
